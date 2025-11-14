2b377b10f77984f77ad34eb74b203b386f6d33b8
Mohammad Yusfi Lakhafidun <mohammadyusfi039@gmail.com>
2025-11-14 15:36:30 +0800

    create model data user, playlist, music

--- main.cpp
+++ main.cpp
@@ -4,6 +4,37 @@
 #include <thread>
 
 using namespace std;
+typedef unsigned int ID_t;
+
+typedef unsigned int ID_t;
+
+struct User {
+    ID_t id;
+    string username;
+    string password;
+    string role;
+    deque<ID_t> playlist_ids;
+};
+
+struct Music {
+    ID_t id;
+    string title;
+    string artist;
+    int duration_seconds;
+};
+
+struct Playlist {
+    ID_t id;
+    string name;
+    string owner_username;
+    deque<ID_t> music_ids;
+};
+
+// penyimpanan data
+deque<User> users = {};
+deque<Music> musics = {};
+deque<Playlist> playlists = {};
+
 
 int main()
 {
